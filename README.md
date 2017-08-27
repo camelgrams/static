0d146b079eef70ea9779cb2c232c8204ce49cfd2
jiangwei <jiangwei@gagogroup.com>
2017-08-27 13:05:47 +0800

    feat: init

---
+++ README.md
@@ -0,0 +1,7 @@
+# 部署
+静态出static文件夹所有文件，入口是index.html
+
+# 开发
+npm install http-server -g 
+http-server static  
+访问index.html
---
+++ js/main.js
@@ -0,0 +1,16 @@
+qr.canvas({
+  canvas: document.getElementById('qr-code'),
+  value: 'http://www.baidu.com'
+});
+qr.canvas({
+  canvas: document.getElementById('qr-code1'),
+  value: 'http://www.baidu.com'
+});
+qr.canvas({
+  canvas: document.getElementById('qr-code2'),
+  value: 'http://www.baidu.com'
+});
+qr.canvas({
+  canvas: document.getElementById('qr-code3'),
+  value: 'http://www.baidu.com'
+});
